9ae028a594a08afd488c14c3d1a15d4977270239
olzhasar <o.arystanov@gmail.com>
2024-02-22 01:43:24 +0600

    day 1 part 2

--- day_1/main.go
+++ day_1/main.go
@@ -24,7 +24,59 @@ func calculateCalibration(str string) int {
 }
 
 func calculateCalibrationPartTwo(str string) int {
-	return 0
+	numberRunes := [][]rune{
+		[]rune("one"),
+		[]rune("two"),
+		[]rune("three"),
+		[]rune("four"),
+		[]rune("five"),
+		[]rune("six"),
+		[]rune("seven"),
+		[]rune("eight"),
+		[]rune("nine"),
+	}
+
+	length := len(str)
+
+	runes := []rune(str)
+
+	getCurrentNumber := func(index int) int {
+		r := runes[index]
+		if unicode.IsDigit(r) {
+			return int(r - '0')
+		}
+
+		checkNumber := func(i int, number []rune) bool {
+			for _, r := range number {
+				if i >= length || runes[i] != r {
+					return false
+				}
+				i++
+			}
+			return true
+		}
+
+		for i, number := range numberRunes {
+			if checkNumber(index, number) {
+				return i + 1
+			}
+		}
+		return -1
+	}
+
+	first := -1
+	last := -1
+
+	for i := 0; i < len(str); i++ {
+		if number := getCurrentNumber(i); number != -1 {
+			if first == -1 {
+				first = number
+			}
+			last = number
+		}
+	}
+
+	return first*10 + last
 }
 
 func main() {
@@ -39,7 +91,7 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		result += calculateCalibration(scanner.Text())
+		result += calculateCalibrationPartTwo(scanner.Text())
 	}
 
 	fmt.Println(result)
