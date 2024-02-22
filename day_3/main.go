b688e3d3d3018535f34a1f22710cf9764c988b21
olzhasar <o.arystanov@gmail.com>
2024-02-22 17:25:34 +0600

    day 3 part 2

--- day_3/main.go
+++ day_3/main.go
@@ -7,111 +7,178 @@ import (
 	"unicode"
 )
 
-type Part struct {
-	x_start int
-	y_start int
-	y       int
-	number  int
+var DIRS = [8][2]int{
+	{-1, 0},
+	{1, 0},
+	{0, -1},
+	{0, 1},
+	{-1, -1},
+	{-1, 1},
+	{1, -1},
+	{1, 1},
 }
 
-func getSymbolMatrix(input []string) [][]bool {
-	length := len(input)
+func getMatrix(input []string) (matrix [][]rune, m int, n int) {
+	m = len(input)
 
-	matrix := make([][]bool, length)
+	matrix = make([][]rune, m)
 
 	for i, row := range input {
-		for _, char := range row {
-			if char == '.' || unicode.IsDigit(char) {
-				matrix[i] = append(matrix[i], false)
-			} else {
-				matrix[i] = append(matrix[i], true)
-			}
-		}
+		matrix[i] = []rune(row)
 	}
 
-	return matrix
+	n = len(matrix[0])
+
+	return matrix, m, n
 }
 
-func getAllParts(input []string) []Part {
-	result := []Part{}
-
-	start := -1
-	end := -1
-
-	appendPart := func(row string, rowNumber int) {
-		number := 0
-
-		for i := start; i <= end; i++ {
-			number = number*10 + int(row[i]-'0')
-		}
-
-		result = append(result, Part{start, end, rowNumber, number})
-
-		start = -1
-		end = -1
-	}
-
-	for i, row := range input {
-		for j, char := range row {
-			if unicode.IsDigit(char) {
-				if start == -1 {
-					start = j
-				}
-				end = j
-			} else {
-				if start != -1 {
-					appendPart(row, i)
-				}
-			}
-		}
-
-		if start != -1 {
-			appendPart(row, i)
-		}
-	}
-
-	return result
+func isSymbol(char rune) bool {
+	return char != '.' && !unicode.IsDigit(char)
 }
 
-func hasAdjacentSymbol(matrix [][]bool, part Part, m int, n int) bool {
-	directions := [8][2]int{
-		{-1, 0},
-		{1, 0},
-		{0, -1},
-		{0, 1},
-		{-1, -1},
-		{-1, 1},
-		{1, -1},
-		{1, 1},
-	}
+func isDigit(char rune) bool {
+	return unicode.IsDigit(char)
+}
 
-	for i := part.x_start; i <= part.y_start; i++ {
-		for _, direction := range directions {
-			x := direction[0] + i
-			y := direction[1] + part.y
+func isStar(char rune) bool {
+	return char == '*'
+}
 
-			if x >= 0 && x < m && y >= 0 && y < n && matrix[y][x] {
-				return true
-			}
+func hasAdjacentSymbol(matrix [][]rune, m int, n int, row int, column int) bool {
+	for _, direction := range DIRS {
+		r := direction[0] + row
+		c := direction[1] + column
+
+		if r >= 0 && r < m && c >= 0 && c < n && isSymbol(matrix[r][c]) {
+			return true
 		}
 	}
-
 	return false
 }
 
 func getPartNumbersSum(input []string) int {
-	M := len(input)
-	N := len(input[0])
+	matrix, m, n := getMatrix(input)
 
-	symbolsMatrix := getSymbolMatrix(input)
-
-	parts := getAllParts(input)
+	fmt.Println("m:", m, "n:", n)
 
 	total := 0
 
-	for _, part := range parts {
-		if hasAdjacentSymbol(symbolsMatrix, part, M, N) {
-			total += part.number
+	current := -1
+	has_adj := false
+
+	for i, row := range matrix {
+		for j, char := range row {
+			if isDigit(char) {
+				val := int(char - '0')
+				if current == -1 {
+					current = val
+				} else {
+					current = current*10 + val
+				}
+				has_adj = has_adj || hasAdjacentSymbol(matrix, m, n, i, j)
+			} else {
+				if current != -1 && has_adj {
+					total += current
+				}
+				current = -1
+				has_adj = false
+			}
+		}
+		if current != -1 && has_adj {
+			total += current
+		}
+		current = -1
+		has_adj = false
+	}
+
+	return total
+}
+
+func getAllNumbers(matrix [][]rune) (numbersMatrix [][]int, numbers []int) {
+	numbersMatrix = make([][]int, len(matrix))
+	numbers = make([]int, 0)
+
+	current := -1
+	index := 0
+
+	for i, row := range matrix {
+		for _, char := range row {
+			if isDigit(char) {
+				val := int(char - '0')
+				if current == -1 {
+					current = val
+				} else {
+					current = current*10 + val
+				}
+				numbersMatrix[i] = append(numbersMatrix[i], index)
+			} else {
+				if current != -1 {
+					numbers = append(numbers, current)
+					index++
+				}
+				current = -1
+				numbersMatrix[i] = append(numbersMatrix[i], -1)
+			}
+		}
+		if current != -1 {
+			numbers = append(numbers, current)
+			index++
+		}
+		current = -1
+	}
+
+	return numbersMatrix, numbers
+}
+
+func calcGearRatio(numbersMatrix [][]int, numbers []int, m int, n int, row int, col int) int {
+	ratio := 1
+	metNumbers := make([]int, 0)
+
+	contains := func(val int) bool {
+		for _, num := range metNumbers {
+			if num == val {
+				return true
+			}
+		}
+		return false
+	}
+
+	for _, direction := range DIRS {
+		r := row + direction[0]
+		c := col + direction[1]
+		index := numbersMatrix[r][c]
+		if r >= 0 && r < m && c >= 0 && c < n && index != -1 {
+			number := numbers[index]
+
+			if !contains(number) {
+				if len(metNumbers) > 1 {
+					return 0
+				}
+				metNumbers = append(metNumbers, number)
+				ratio *= number
+			}
+		}
+	}
+
+	if len(metNumbers) != 2 {
+		return 0
+	}
+
+	return ratio
+}
+
+func getGearRatioSum(input []string) int {
+	matrix, m, n := getMatrix(input)
+
+	numbersMatrix, numbers := getAllNumbers(matrix)
+
+	total := 0
+
+	for i, row := range matrix {
+		for j, char := range row {
+			if isStar(char) {
+				total += calcGearRatio(numbersMatrix, numbers, m, n, i, j)
+			}
 		}
 	}
 
@@ -133,7 +200,7 @@ func main() {
 		input = append(input, scanner.Text())
 	}
 
-	result := getPartNumbersSum(input)
+	result := getGearRatioSum(input)
 
 	fmt.Println(result)
 }
--- day_3/main_test.go
+++ day_3/main_test.go
@@ -24,3 +24,26 @@ func TestGetPartNumbersSum(t *testing.T) {
 		t.Errorf("Expected %d, got %d", expected, result)
 	}
 }
+
+func TestGetGearRatioSum(t *testing.T) {
+	exampleInput := []string{
+		"467..114..",
+		"...*......",
+		"..35..633.",
+		"......#...",
+		"617*......",
+		".....+.58.",
+		"..592.....",
+		"......755.",
+		"...$.*....",
+		".664.598..",
+	}
+
+	expected := 467835
+
+	result := getGearRatioSum(exampleInput)
+
+	if result != expected {
+		t.Errorf("Expected %d, got %d", expected, result)
+	}
+}
